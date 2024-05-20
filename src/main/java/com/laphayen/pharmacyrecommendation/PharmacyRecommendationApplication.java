52475a12334364edfe76c6c595656aac693f1880
Baek Gi Chan <laphayen@gmail.com>
2024-05-20 23:01:46 +0900

    #1 - Create Spring Boot project
    Initialize the root directory as a Spring Boot project and configure the development environment.

---
+++ src/main/java/com/laphayen/pharmacyrecommendation/PharmacyRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.laphayen.pharmacyrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyRecommendationApplication.class, args);
+	}
+
+}
